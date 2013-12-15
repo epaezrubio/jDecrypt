451d100690504a47b94be629c0ad7a84ec9ced15
Eduardo Páez <epaezrubio@gmail.com>
2013-12-15 15:47:52 +0100

    Ceasar cipher and regexp adjusted

--- src/jDecrypt.js
+++ src/jDecrypt.js
@@ -4,8 +4,12 @@ var jDecrypt = function() {
 	that.languaje
 	that.encryption
 
+	String.prototype.replaceAt = function(index, character) {
+		return this.substr(0, index) + character + this.substr(index+character.length);
+	}
+
 	var setText = function(text) {
-		that.text = text;
+		that.text = text.toUpperCase();
 	}
 
 	var getLanguage = function(text) {
@@ -24,7 +28,7 @@ var jDecrypt = function() {
 
 		/* GERMAN */
 
-		if (that.text.match(new RegExp("[ß]"))) {
+		if (that.text.match(new RegExp("ß"))) {
 			return "german";
 		}
 
@@ -32,5 +36,20 @@ var jDecrypt = function() {
 
 	}
 
-	return {setText:setText, getLanguage:getLanguage}
+	var caesar = function(text, shift) {
+		var upperCaseText = text.toUpperCase() || that.text || "";
+		shift = (typeof shift !== "undefined") ? (shift % 25) : 1;
+		for (var i = 0; i < upperCaseText.length; i++) {
+			var charCode = upperCaseText.charCodeAt(i);
+			if (charCode >= 65 && charCode <= 90) {
+				if ((charCode + shift) < 65) {
+					charCode = charCode + 26;
+				}
+				upperCaseText = upperCaseText.replaceAt(i, String.fromCharCode(charCode + shift));
+			}
+		}
+		return upperCaseText;
+	}
+
+	return {setText:setText, getLanguage:getLanguage, caesar:caesar}
 }
--- test/tests.js
+++ test/tests.js
@@ -15,3 +15,18 @@ test("Languaje: German", function() {
 	germanDecrypt.setText("FßFO")
 	equal(germanDecrypt.getLanguage(), "german");
 })
+test("Caesar", function() {
+	var myDecrypt = jDecrypt();
+	equal(myDecrypt.caesar("ABC"), "BCD");
+	equal(myDecrypt.caesar("ABC", 2), "CDE");
+	equal(myDecrypt.caesar("ABC", 27), "CDE");
+	equal(myDecrypt.caesar("ABC", -1), "ZAB");
+	equal(myDecrypt.caesar("ABC", 25), "ABC");
+	equal(myDecrypt.caesar("ABC", -25), "ABC");
+	equal(myDecrypt.caesar("ABC", -50), "ABC");
+	equal(myDecrypt.caesar("ABC", -51), "ZAB");
+	equal(myDecrypt.caesar("BCD", -1), "ABC");
+	equal(myDecrypt.caesar("BCD?", -1), "ABC?");
+	equal(myDecrypt.caesar("bce", -1), "ABD");
+	equal(myDecrypt.caesar("ace", -1), "ZBD");
+})
