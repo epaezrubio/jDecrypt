29a6fb866eda984dadb29be76ee638a9942e0798
Eduardo Páez <epaezrubio@gmail.com>
2013-12-15 17:47:56 +0100

    Better test description

--- test/tests.js
+++ test/tests.js
@@ -3,30 +3,30 @@ test("QUnit working", function() {
 })
 test("Languaje: Spanish", function() {
 	var spanishDecrypt = jDecrypt();
-	spanishDecrypt.setText("IPMB, ¿RVF UBM?")
+	spanishDecrypt.setText("IPMB, ¿RVF UBM?", "¿ Character Detection")
 	equal(spanishDecrypt.getLanguage(), "spanish");
-	spanishDecrypt.setText("BÑP RVF WJFOF")
+	spanishDecrypt.setText("BÑP RVF WJFOF", "Ñ Character Detection")
 	equal(spanishDecrypt.getLanguage(), "spanish");
-	spanishDecrypt.setText("IPMB, RVF UBM")
+	spanishDecrypt.setText("IPMB, RVF UBM", "Spanish Detectioin")
 	equal(spanishDecrypt.getLanguage(), "spanish");
 })
 test("Languaje: German", function() {
 	var germanDecrypt = jDecrypt();
-	germanDecrypt.setText("FßFO")
-	equal(germanDecrypt.getLanguage(), "german");
+	germanDecrypt.setText("FßFO");
+	equal(germanDecrypt.getLanguage(), "german", "ß Character Detected");
 })
 test("Caesar", function() {
 	var myDecrypt = jDecrypt();
-	equal(myDecrypt.caesar("ABC"), "BCD");
-	equal(myDecrypt.caesar("ABC", 2), "CDE");
-	equal(myDecrypt.caesar("ABC", 27), "CDE");
-	equal(myDecrypt.caesar("ABC", -1), "ZAB");
-	equal(myDecrypt.caesar("ABC", 25), "ABC");
-	equal(myDecrypt.caesar("ABC", -25), "ABC");
-	equal(myDecrypt.caesar("ABC", -50), "ABC");
-	equal(myDecrypt.caesar("ABC", -51), "ZAB");
-	equal(myDecrypt.caesar("BCD", -1), "ABC");
-	equal(myDecrypt.caesar("BCD?", -1), "ABC?");
-	equal(myDecrypt.caesar("bce", -1), "ABD");
-	equal(myDecrypt.caesar("ace", -1), "ZBD");
+	equal(myDecrypt.caesar("ABC"), "BCD", "Simple Ceasar");
+	equal(myDecrypt.caesar("ABC", 2), "CDE", "Custom Shift");
+	equal(myDecrypt.caesar("ABC", 27), "CDE", "Shift Loop");
+	equal(myDecrypt.caesar("ABC", -1), "ZAB", "Negative Shift");
+	equal(myDecrypt.caesar("ABC", 25), "ABC", "Complete Loop");
+	equal(myDecrypt.caesar("ABC", -25), "ABC", "Negative Complete Loop");
+	equal(myDecrypt.caesar("ABC", -50), "ABC", "Double Loop");
+	equal(myDecrypt.caesar("ABC", -51), "ZAB", "Double Negative Loop");
+	equal(myDecrypt.caesar("BCD", -1), "ABC", "One Simple More");
+	equal(myDecrypt.caesar("BCD?", -1), "ABC?", "Testing Special Chars");
+	equal(myDecrypt.caesar("bce", -1), "ABD", "Testing Upper Case");
+	equal(myDecrypt.caesar("ace", -1), "ZBD", "Upper Case and Loop");
 })
